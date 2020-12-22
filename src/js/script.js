512b72f16a548f2f4eb63c05023ccd795ae1634b
Andrei <voynenko.andrey@gmail.com>
2020-12-22 21:28:52 +0200

    added smooth scroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //Slider
     $('.testimonials__inner').slick({
         speed: 1000,
         autoplay: true,
@@ -6,4 +7,19 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/prev_arrow.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/next_arrow.png"></button>'
       });
+
+    //Smooth scroll
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#up']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+});
   });
