99b94733dbe7a2f5b1ebee34051b37fee2e82389
Andrei <voynenko.andrey@gmail.com>
2020-12-22 21:10:08 +0200

    create repository

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.testimonials__inner').slick({
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/prev_arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/next_arrow.png"></button>'
+      });
+  });
